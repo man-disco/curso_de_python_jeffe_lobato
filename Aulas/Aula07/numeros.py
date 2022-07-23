25c74bbb84c42e7e2d6817ccba058881a102e0ea
man-disco <109635328+man-disco@users.noreply.github.com>
2022-07-22 21:55:53 -0300

    Add files via upload

---
+++ Aulas/Aula07/numeros.py
@@ -0,0 +1,27 @@
+soma = 2 + 3
+subtracao = 3 - 2
+mult = 3 * 2
+div = 3/2
+exp = 3 ** 2
+
+print(soma)
+print(subtracao)
+print(mult)
+print(div)
+print(exp)
+
+# Exercício
+# Python segue a ordem da matemática.
+# Ex: Parêntesis; Multiplicação primeiro, etc.
+oper = (2 + 3)*4
+oper2 = 7 / 2 + 4
+print(oper)
+print(oper2)
+
+# Exercício
+palavra1 = "Olá"
+palavra2 = ", eu tenho "
+palavra3 = " anos."
+idade = 32
+
+print(palavra1 + palavra2 + str(idade) + palavra3)
