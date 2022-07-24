54a5e784bf587006049d2679546dbff0b29f7e97
man-disco <109635328+man-disco@users.noreply.github.com>
2022-07-23 23:25:45 -0300

    Add files via upload

---
+++ Aulas/Aula09/listas.py
@@ -0,0 +1,11 @@
+carros = ['ferrari', 'fusca', 'civic']
+carros.insert(1, 'jeep',)
+carros.append('BMW')
+carros[2] = 'mustang'
+print(carros)
+
+# Organizando listas.
+print(carros.sort())
+print(len(carros))
+carros.reverse()
+print(carros)
