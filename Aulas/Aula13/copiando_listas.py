43b53cfd71ed84aa5d64927fa2d924adf309fa2d
man-disco <109635328+man-disco@users.noreply.github.com>
2022-07-26 21:50:50 -0300

    Add files via upload

---
+++ Aulas/Aula13/copiando_listas.py
@@ -0,0 +1,8 @@
+alimentos = ['Tomate', 'Batata', 'Uva', 'Banana', 'Ovos', 'Pão', 'Pizza']
+alimentos2 = alimentos[::]
+
+alimentos.insert(1, 'Hamburger')
+
+alimentos.sort()
+print(alimentos)
+print(alimentos2)
---
+++ Aulas/Aula14/tuplas.py
@@ -0,0 +1,11 @@
+# Tuplas são listas definidas que não podem ser alteradas.
+alimentos = ('Hot-dog,', 'Hamburger', 'Batata Frita')
+print(alimentos[0])
+
+for alimento in alimentos:
+    print(alimento)
+
+
+numeros = [3, 2, 1]
+for num in numeros:
+    print(num)
