bb7f368515b28f23162fe8b6e984c474b7a9ac64
man-disco <109635328+man-disco@users.noreply.github.com>
2022-07-27 22:23:03 -0300

    Add files via upload

---
+++ Aulas/Aula15/e_se.py
@@ -0,0 +1,15 @@
+cor = 'Verde'
+num = 32
+if num == 1:
+
+    if cor == 'Azul':
+        print(cor)
+
+    elif cor == 'Amarelo':
+        print('Cor Amarela.')
+
+    else:
+        print('Cor não é azul nem amarelo.')
+
+else:
+    print('Não executei os ifs anteriores')
