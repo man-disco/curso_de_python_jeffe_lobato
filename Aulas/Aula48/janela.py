920dcee93116fd2bf65c8aca1b2b6a86a48b916e
john <jlvj@tutanota.com>
2022-08-28 14:29:59 -0300

    Update

--- Aulas/Aula48/janela.py
+++ Aulas/Aula48/janela.py
@@ -1,1 +1,27 @@
-##
+from tkinter import *
+
+
+class Janela:
+    def __init__(self, raiz):
+        self.fr1 = Frame(raiz)
+        self.fr1.pack()
+        self.lb1 = Label(self.fr1, text="Olá mundo", background="grey", font=('Liberation', 23))
+        self.lb1.pack(side=LEFT)
+
+        self.fr2 = Frame(raiz)
+        self.fr2.pack()
+
+        self.bt1 = Button(self.fr2, text='Clique aqui!', background="#8bb158", font=('Times', 23, 'italic'))
+        self.bt1.pack(side=LEFT)
+
+        self.lb2 = Label(self.fr1, text='   ', width=100)
+        self.lb2.pack(side=RIGHT)
+
+        self.lb2 = Label(self.fr2, text='   ', width=100)
+        self.lb2.pack(side=RIGHT)
+
+
+raiz = Tk()
+Janela(raiz)
+raiz.geometry("800x600+0+0")
+raiz.mainloop()
