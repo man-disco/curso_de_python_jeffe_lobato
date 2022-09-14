444f8f3714f6c62202a4417c99dcfe223a1038a7
john <jlvj@tutanota.com>
2022-09-14 18:26:22 -0300

    Update

---
+++ AlienInvasion/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Uma classe para armazenar todas as configurações da invasão alienígena."""
+    def __init__(self):
+        """Inicializa as configurações do jogo."""
+        # Configurações da resolução.
+        self.screen_width = 800
+        self.screen_height = 600
+        # Cor de fundo da janela.
+        self.bg_color = (30, 30, 30)
